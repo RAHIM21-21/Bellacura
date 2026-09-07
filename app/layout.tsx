8a3df38f976a336de0b66750e8f64b3450880951
RAHIM21-21 <rahimeladnani@gmail.com>
2026-09-07 16:53:42 +0000

    fix: use single-line Meta Pixel script to avoid template literal parsing issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,20 +65,8 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema()) }}
         />
-        <script
-          dangerouslySetInnerHTML={{ __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1055883607362457');
-            fbq('track', 'PageView');
-          ` }}
-        />
+        <script dangerouslySetInnerHTML={{ __html: "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1055883607362457');fbq('track','PageView');" }} />
+        <noscript><img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=1055883607362457&ev=PageView&noscript=1" alt="" /></noscript>
       </head>
       <body className="min-h-screen flex flex-col bg-cream">
         <Header />
